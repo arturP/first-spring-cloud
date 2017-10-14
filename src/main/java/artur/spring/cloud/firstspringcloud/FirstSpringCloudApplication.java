4304e128ef2ba92143026c357ff8333cad177977
U-DESKTOP-26Q1HDR\Artur <artur2p@gmail.com>
2017-10-14 18:36:30 +0200

    inital

---
+++ src/main/java/artur/spring/cloud/firstspringcloud/FirstSpringCloudApplication.java
@@ -0,0 +1,12 @@
+package artur.spring.cloud.firstspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringCloudApplication.class, args);
+	}
+}
